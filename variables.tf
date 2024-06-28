3fc6164cf4fffc4aec48a15209abc66fcc627030
bruno5barros <67475745+bruno5barros@users.noreply.github.com>
2024-06-28 13:08:15 +0200

    create and user vars.

--- variables.tf
+++ variables.tf
@@ -2,3 +2,45 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
 }
+
+variable "ami_filter" {
+  description = "Name filter and owner for AMI."
+    
+  type = object({
+    name  = string
+    owner = string
+  })
+    
+  default = {
+    values = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+    owner  = "979382823631"  # Bitnami
+  }
+}
+
+variable "environment" {
+  description = "Development environment."
+
+  type = object({
+    name           = string
+    network_prefix = string
+  })
+
+  default = {
+    name           = "dev"
+    network_prefix = "10.0."
+  }
+}
+
+variable "blog_autoscaling" {
+  default = "Minimum and maximum number of instances in the ASG."
+
+  type = object({
+    min_size = number
+    max_size = number
+  })
+
+  default = {
+    min_size = 1
+    max_size = 2
+  }
+}
